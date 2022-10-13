1249901fece9fca998158c4023a2209b70869846
Kartikeybharti8 <kartikeybharti8@gmail.com>
2022-10-13 23:27:49 +0530

    explaining sheetal git work

--- api/index.js
+++ api/index.js
@@ -28,6 +28,7 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 //hello
+// explain sheetal
 app.get("/",(eq,res)=>{
   res.end("home")
 })
