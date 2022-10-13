8fd2b111e3720be242c60746a49e14a649547ee0
Kartikeybharti8 <kartikeybharti8@gmail.com>
2022-10-13 15:22:35 +0530

    jai hind

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
-
+//hello
 app.get("/",(eq,res)=>{
   res.end("home")
 })
