7494075485ad0b87ec3f33d448be8566a2e12cd0
Sheetal Sharma <sharmasheetal01761@gmail.com>
2022-12-03 00:43:04 +0530

    Update data.js

--- client/src/data.js
+++ client/src/data.js
@@ -7,7 +7,6 @@ export const sliderItems = [
     //sales
     desc: "Some of the best selling products are on sale",
     bg: "FFFFF",
-
   },
   {
     id: 2,
@@ -29,8 +28,6 @@ export const sliderItems = [
     title: "AUTHENTIC WEAR",
     desc: "GET FLAT 30% OFF FOR NEW ARRIVALS",
     bg: "FFFFF",
-
-
   },
 ];
 
@@ -39,19 +36,19 @@ export const categories = [
     id: 1,
     img: "https://cdn.shopify.com/s/files/1/0155/8131/products/0Q8A6927_480x.jpg?v=1664965442",
     title: "POTTERY",
-    cat: "POTTERY"
+    cat: "POTTERY",
   },
   {
     id: 2,
     img: "https://cdn.shopify.com/s/files/1/0155/8131/collections/BeFunky-collage_20_e1f0333f-dc80-40d2-9d5c-b2a0d4a9c4dc_1500x.jpg?v=1665231347",
     title: "CLOTHING",
-    cat: "CLOTHING"
+    cat: "CLOTHING",
   },
   {
     id: 3,
     img: "https://cdn.shopify.com/s/files/1/0155/8131/collections/sample-nitin-collection-thumb_ea22ef0a-c54c-4ad4-b1a9-00d1e677d7b8.jpg?v=1639131115&width=200",
     title: "JEWELERY",
-    cat: "JEWELERY"
+    cat: "JEWELERY",
   },
 ];
 
@@ -65,7 +62,7 @@ export const popularProducts = [
     // size: [],
     // color: ["blue", "black"],
     price: 600,
-    inStock: true
+    inStock: true,
   },
   {
     id: "634798a0df4c8b863b4bd6f9",
@@ -76,7 +73,7 @@ export const popularProducts = [
     // size: [],
     // color: ["blue", "black"],
     price: 800,
-    inStock: true
+    inStock: true,
   },
   {
     id: "634798fddf4c8b863b4bd6fb",
@@ -87,7 +84,7 @@ export const popularProducts = [
     // size: [],
     // color: ["blue", "black"],
     price: 800,
-    inStock: true
+    inStock: true,
   },
   {
     id: "63479a99df4c8b863b4bd6fd",
@@ -98,7 +95,7 @@ export const popularProducts = [
     // size: [],
     // color: ["blue", "black"],
     price: 800,
-    inStock: false
+    inStock: false,
   },
   {
     id: "63479eafdf4c8b863b4bd701",
@@ -109,7 +106,7 @@ export const popularProducts = [
     // size: [],
     // color: ["blue", "black"],
     price: 500,
-    inStock: true
+    inStock: true,
   },
   {
     id: "6347a598df4c8b863b4bd708",
@@ -120,7 +117,7 @@ export const popularProducts = [
     // size: [],
     // color: ["blue", "black"],
     price: 800,
-    inStock: true
+    inStock: true,
   },
   {
     id: "6347a5c6df4c8b863b4bd70a",
@@ -131,7 +128,7 @@ export const popularProducts = [
     // size: [],
     // color: ["blue", "black"],
     price: 400,
-    inStock: false
+    inStock: false,
   },
   {
     id: "6347a626df4c8b863b4bd70c",
@@ -142,6 +139,330 @@ export const popularProducts = [
     // size: [],
     // color: ["blue", "black"],
     price: 760,
-    inStock: true
+    inStock: true,
   },
-]
+
+  //sheetal's added products
+
+  {
+    id: "6347a626df4c8b863b4bd72c",
+    img: "https://cdn.shopify.com/s/files/1/0155/8131/products/Q8A9780_2048x.jpg?v=1663419368",
+    title: "TERRACOTTA ROUND 7-WICK DIYA",
+    desc: `Terracotta Round 7-wick Diya
+            Handmade Terracotta Diyo
+            7-wick Diwali Puja Diyo
+            Color: Reddish Brown
+            Diameter: Approx. 21 cm
+            Weight: 520g`,
+    categories: ["POTTERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 50,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd72c",
+    img: "https://wudbox.in/wp-content/uploads/2018/11/P002-1.jpg",
+    title: "RAJASTHANI HANDCRAFTED BLUE POTTERY WALL DÉCOR PLATE -8 INCHES",
+    desc: `Blue pottery art is basically originated from Turkey and Persia but now it’s very well known and recognized art from Jaipur the Pink City of Rajasthan.
+
+This art has got Blue Pottery name because of Cobalt Blue dye used to color the pottery items. It is one of many Eurasian types of blue and white pottery, and related in the shapes and decoration to Islamic pottery and, more distantly, Chinese pottery.
+
+Give your dull wall space an instant face-lift, with this beautiful hand-painted decorative wall plate. Although it is a classic wall décor, this plate may also be used as a festive platter.
+
+This product has been handcrafted by skilled artisans. The visible brush strokes and neat free-hand curves are the artistic expressions of our craftsmen, which cannot be reproduced in digital print. Color shades, pattern and polish may vary slightly around the surface, since the painting is done free-style.
+
+SPECIFICATIONS:     
+
+Color- Multicolor
+Material- Quartz
+Art- Blue Pottery
+Weight- 350gm
+Dimensions- 8 Inch
+Deliver in 5-6 business days
+Perfect for Wall Décor`,
+    categories: ["POTTERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 100,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd73c",
+    img: "https://cdn.shopify.com/s/files/1/0537/3607/9537/products/CAG.012_0552_2048x2048.jpg?v=1622728327",
+    title: "HANDMADE TERRACOTTA WOOL BOWL",
+    desc: `This product is handcrafted by talented artisans, one piece at a time, making each piece unique. Slight variations are inevitable and actually make every piece bespoke. Such variations are not considered a defect.
+Material: Ceramic Stoneware
+Diameter 19cm, Height 9cm
+Use and Care
+The bowl can be put in a dishwasher.
+We recommend handwashing.
+Do handle the swirl in the bowl with love.
+Hand wash gently with a mild soap and use a soft brush when necessary.
+Do not use harsh detergents and abrasive scrubbers.`,
+    categories: ["POTTERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 100,
+    inStock: true,
+  },
+
+  {
+    id: "6347a626df4c8b863b4bd74c",
+    img: "https://wudbox.in/wp-content/uploads/2018/11/SV002.jpg",
+    title: "BLUE POTTERY CERAMIC LEAF VASE",
+    desc: `SKU :SV002
+            Material :Multani mitti with glass
+            Dimensions (Inch): 5 L x 5 W x 8 H
+            Pack Content: 1 Flower Vase`,
+    categories: ["POTTERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd75c",
+    img: "https://cdn.shopify.com/s/files/1/2116/1923/products/SACHII009_1.jpg?v=1607327373",
+    title: "TERRACOTTA TEA CUP",
+    desc: "POTTERY",
+    categories: ["POTTERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 200,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd76c",
+    img: "https://cdn.shopify.com/s/files/1/0155/8131/products/MG_2370_f4c930b5-0608-4f8a-8c25-6c79c46097d4_2048x.jpg?v=1667982295",
+    title: "HANDMADE BEADS & THREADWORK EARRINGS",
+    desc: "POTTERY",
+    categories: ["POTTERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd77c",
+    img: "https://cdn.shopify.com/s/files/1/2116/1923/products/A276D879-C972-4DDB-AF68-088E97EBE340.jpg?v=1612748724",
+    title: "MAROON BEIGE TASSEL COIN BOX NECKLACE",
+    desc: `The rosegold box chain necklace has a combination of maroon, red, earthy greens, sage green, gray tassels along with embossed brass coins that add to the charm.
+It is a very versatile and a subtle neckpiece. Perfect to style it with different types of attires.
+Length : 38cm + 5 cm extender.`,
+    categories: ["JEWELERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd78c",
+    img: "https://cdn.shopify.com/s/files/1/2116/1923/products/NEK064-B-3.jpg?v=1642849702",
+    title: "SAMOOLAM AADYA NECKLACE ~ RED OMBRE",
+    desc: `Let this four-layered Samoolam necklace with colourful crochet beads and chain add a dash of drama to your looks. Contrast it with a solid-coloured ensemble or match it with a printed separate.
+            Material: Viscose Thread, Cotton Thread, Wooden Bead, Glass Bead, Brass
+            Length (cm): 150 | Width (cm): 5
+            Colour: Red
+            
+            Disclaimer: Each piece is handmade. There will be a slight variation in colour, shape and size. The threads used in this piece are hand dyed and might bleed if washed. Dry Clean Only.`,
+    categories: ["JEWELERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd79c",
+    img: "https://cdn.shopify.com/s/files/1/2116/1923/products/BE3B0A46-9B23-4C20-9C68-7A929F027270.jpg?v=1602918097",
+    title: "NAVY WINE GRAY TASSEL COIN BOX NECKLACE",
+    desc: "The rosegold box chain necklace has a combination of navy, wine, gray tassels along with embossed brass coins that add to the charm. It is a very versatile and a subtle neckpiece. Perfect to style it with different types of attires. Length : 38cm + 5 cm extender.",
+    categories: ["JEWELERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd80c",
+    img: "https://cdn.vibecity.in/providers/61f013492300fa00118b6673/010037e2-a286-4a1a-a357-09e247c945af_5b998e02-3842-4a16-97d7-8565aa730a58-3X.webp",
+    title: "BLUE POTTERY HANDCRAFTED 'COTTON JAR'",
+    desc: `Blue Pottery is a traditional craft of Turko-Persian region, it’s seed was planted in Jaipur, Rajasthan (India), in the 19th century. Since then Blue Pottery has bloomed and flourished exclusively in Jaipur. It was registered under Geographical Indication handicrafts in 2009 and is popularly known as “Jaipur Blue Pottery”. Quartz stone and glass as it’s main ingredients, Blue Pottery uses low-fired glaze, which makes it unique and markedly distinct from other forms of pottery. Each item of Blue Pottery goes through 42 manual processes over a period of 20-25 days until the finished product is ready. Blue Pottery is known for it’s lustre. Unlike ceramics, it’s sheen last for a minimum of 800 years!". Shilp Guru Gopal Saini is a president awarded artist. With untiring and sustained painstaking efforts of whom, the art of Blue Pottery has gained new momentum and has reached to unfathomable heights of excellence. Shri Saini has been awarded numerous awards. To name few; shilp Guru award (2016)ministry of textiles, Government of India; national award (2009) , ministry of textiles, Government of India. Material - Quartz Powder, Glass Powder, Katira | Color - Lignt Blue| Size - L- 3inches, B-3 inches, H-3.5 inches | Product Care Instructions - Handle with Care- FRAGILE, Breaks easily on dropping, no microwave, no dishwasher . Product is ready to ship
+
+Note : Blue Pottery Products are suitable for storing dry things only.`,
+    categories: ["POTTERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd81c",
+    img: "https://cdn.shopify.com/s/files/1/2105/0147/products/20200630_132030_823x.jpg?v=1603363027",
+    title: "TERRACOTTA GLASSES - SET OF 2",
+    desc: `Handcrafted set of terracotta glasses with beautiful details of mirror artwork. Once a piece is finished by hand, it may result in subtle differences in the handle or rim. You will find varying thickness on each piece which imparts subtle color and texture variations. These variations, rather than being imperfect or flawed, provide unique qualities inherent in handmade pottery. Each piece is "Unique"
+
+Handcrafted in India
+Crafted Consciously and Ethically
+100% Natural, Organic and Eco-friendly
+Capacity : 200 ML
+Dimensions: 10cm(Height) x 5cm(Base)
+
+Maintenance & Care:
+
+Soak terracotta glasses overnight before you start using them
+Wash with mild detergent, no harsh chemicals to be used
+Handmade with Love.  Buy Local.  Support Local.`,
+    categories: ["POTTERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd82c",
+    img: "https://img.tatacliq.com/images/i8/658Wx734H/MP000000013529352_658Wx734H_202206252337541.jpeg",
+    title: "KALAPI HANDPAINTED POTTERY JAR",
+    desc: "POTTERY",
+    categories: ["POTTERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd83c",
+    img: "https://cdn.shopify.com/s/files/1/0155/8131/products/X2A2609_68237d2b-f7d6-425b-ae30-4b6a4dc25257.jpg?v=1669032831&width=450",
+    title: "SANGANERI BLOCK PRINTING COTTON FABRIC",
+    desc: "POTTERY",
+    categories: ["CLOTHING"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+
+  {
+    id: "6347a626df4c8b863b4bd84c",
+    img: "https://cdn.shopify.com/s/files/1/0155/8131/products/X2A2444.jpg?v=1669026396&width=450",
+    title: "MAROON BLACK IVORY AJRAKH HAND BLOCK PRINTED COTTON FABRIC ",
+    desc: "POTTERY",
+    categories: ["CLOTHING"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd85c",
+    img: "https://cdn.shopify.com/s/files/1/0155/8131/products/sareenew_226of1_480x.jpg?v=1659015312",
+    title: "INDIGO BLOCK PRINTED COTTON FULL-SLEEVE KIDS SHIRT",
+    desc: "POTTERY",
+    categories: ["CLOTHING"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd86c",
+    img: "https://cdn.shopify.com/s/files/1/0155/8131/products/0X2A0121_effa9332-74fd-475a-8971-5d8a8a1774bc.jpg?v=1634466456&width=450",
+    title: "CHIKANKARI HAND EMBROIDERED GEORGETTE SHORT FLARED KURTA / TOP",
+    desc: "POTTERY",
+    categories: ["CLOTHING"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd87c",
+    img: "https://cdn.shopify.com/s/files/1/0155/8131/products/E9A9539.jpg?v=1667821604&width=450",
+    title: "MERINO WOOL AJRAKH BLOCK PRINTING HANDLOOM FABRIC",
+    desc: "POTTERY",
+    categories: ["POTTERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+
+  {
+    id: "6347a626df4c8b863b4bd88c",
+    img: "https://cdn.shopify.com/s/files/1/0155/8131/products/IMG_8087_6d72ff17-6fd5-4dab-85d3-218421ec7e25.jpg?v=1634206362&width=450",
+    title: "KUTCH HANDWOVEN BANDHANI MULTI FUMAN PURE WOOLEN STOLE",
+    desc: "POTTERY",
+    categories: ["CLOTHING"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd89c",
+    img: "https://cdn.shopify.com/s/files/1/0155/8131/products/3T1A6489.jpg?v=1637318075&width=450",
+    title: "HANDPAINTED WOODEN BANGLE",
+    desc: "POTTERY",
+    categories: ["JEWELERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd90c",
+    img: "https://cdn.shopify.com/s/files/1/0155/8131/products/E9A9584_2048x.jpg?v=1667822029",
+    title: "AJRAKH BLOCK PRINTING NATURAL DYED COTTON FABRIC",
+    desc: "POTTERY",
+    categories: ["CLOTHING"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd91c",
+    img: "https://cdn.shopify.com/s/files/1/0155/8131/products/MG_4609_bc88e845-47ed-4aef-a2e4-401fec81a81d.jpg?v=1657009145&width=450",
+    title: "FLORAL HANDPAINTED WOODEN BANGLE",
+    desc: "POTTERY",
+    categories: ["JEWELERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd92c",
+    img: "https://cdn.shopify.com/s/files/1/0155/8131/products/MG_0024_71bf8a52-800b-49cf-a0ad-a6f36be71756.jpg?v=1668251631&width=450",
+    title: "SILVER STUDDED JHUMKA EARRINGS",
+    desc: "POTTERY",
+    categories: ["JEWELERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+
+  {
+    id: "6347a626df4c8b863b4bd91c",
+    img: "https://cdn.shopify.com/s/files/1/0155/8131/products/X2A0983_0eaf264b-f044-43bd-943b-cad2b18e2c27.jpg?v=1660476185&width=750",
+    title: "RAAS - MADHUBANI HANDPAINTED WOODEN RING",
+    desc: "POTTERY",
+    categories: ["JEWELERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+  {
+    id: "6347a626df4c8b863b4bd92c",
+    img: "https://cdn.shopify.com/s/files/1/0155/8131/products/MG_9996_010f5293-e1a5-4730-9ea6-93148acc6c1d_2048x.jpg?v=1667634905",
+    title: "LEHERIYA TIE-DYE MOTHRA TUSSAR SILK HANDLOOM STOLE WITH TASSELS",
+    desc: "This wavy-pattern stole has been crafted by the master artisan family of Badshah Miyan from the state of Rajasthan.",
+    categories: ["JEWELERY"],
+    // size: [],
+    // color: ["blue", "black"],
+    price: 600,
+    inStock: true,
+  },
+];
